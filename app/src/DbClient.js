512d025998b5d139fb63434eede8e27b5fb539c0
Pawel <pawel.pietruszka99@gmail.com>
2020-03-28 12:06:43 +0100

    pre flight config

--- app/src/DbClient.js
+++
@@ -1,9 +0,0 @@
-const baseUrl = 'http://localhost:3000'
-
-module.exports = {
-  getAllBoroughs: async () => {
-    return await fetch(baseUrl).then(response => {
-      return response.json();
-    }).catch(err => console.log(err));
-  }
-};
--- server/client/src/ApiClient.js
+++ server/client/src/ApiClient.js
@@ -1,9 +1,7 @@
 const baseUrl = 'http://localhost:3000/boroughs'
 
-module.exports = {
-  getAllBoroughs: async () => {
-    return await fetch(baseUrl).then(response => {
-      return response.json();
-    }).catch(err => console.log(err));
-  }
-};
+export async function getAllBoroughs() {
+  return await fetch(baseUrl).then(response => {
+    return response.json();
+  }).catch(err => console.log(err));
+}
